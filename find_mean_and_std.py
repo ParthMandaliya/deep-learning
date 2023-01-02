12c50315e24e577560dce3fab10003a64b957f8c
Parth Mandaliya <parth.mandaliya.007@gmail.com>
2023-01-02 20:03:51 +0530

    Refactored find_dataset_mean_and_std method

--- find_mean_and_std.py
+++ find_mean_and_std.py
@@ -1,22 +1,14 @@
-import torch
-import torchvision.datasets as datasets
-from torchvision import transforms
+def get_dataset_mean_and_std(dataloader):
+    channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+    for data, _ in dataloader:
+        # Mean over batch, height and width, but not over the channels
+        channels_sum += torch.mean(data, dim=[0,2,3])
+        channels_squared_sum += torch.mean(data**2, dim=[0,2,3])
+        num_batches += 1
 
-cifar_trainset = datasets.CIFAR10(root='./data', train=True, \
-                                  download=True, transform=transforms.ToTensor())
+    mean = channels_sum / num_batches
 
-imgs = [item[0] for item in cifar_trainset] # item[0] and item[1] are image and its label
-imgs = torch.stack(imgs, dim=0).numpy()
+    # std = sqrt(E[X^2] - (E[X])^2)
+    std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
 
-# calculate mean over each channel (r,g,b)
-mean_r = imgs[:,0,:,:].mean()
-mean_g = imgs[:,1,:,:].mean()
-mean_b = imgs[:,2,:,:].mean()
-print(mean_r,mean_g,mean_b)
-
-# calculate std over each channel (r,g,b)
-std_r = imgs[:,0,:,:].std()
-std_g = imgs[:,1,:,:].std()
-std_b = imgs[:,2,:,:].std()
-
-print(std_r, std_g, std_b)
+    return mean, std
